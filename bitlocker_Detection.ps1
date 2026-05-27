6c309f8042ea205a85c1537b3c87946827cb1211
kgsutula <kgsutula@gmail.com>
2026-05-27 10:00:15 -0700

    Add files via upload

---
+++ bitlocker_Detection.ps1
@@ -0,0 +1,14 @@
+﻿$BitLocker = Get-BitLockerVolume -MountPoint "C:"
+
+if (
+    $BitLocker.ProtectionStatus -eq "On" -and
+    $BitLocker.VolumeStatus -eq "FullyEncrypted"
+)
+{
+    Write-Output "Detected"
+    exit 0
+}
+else
+{
+    exit 1
+}
